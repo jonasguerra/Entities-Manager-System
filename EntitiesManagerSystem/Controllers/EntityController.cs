1fd787a809e8e9bc0a463261c348cdecf33f99a4
João Pedro Kaspary <joaopedrokaspary@hotmail.com>
2019-04-06 18:07:36 -0300

    commit

---
+++ EntitiesManagerSystem/Controllers/EntityController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace EntitiesManagerSystem.Controllers
+{
+    public class EntityController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.user = "entity";
+            ViewBag.index = "active";
+            
+            return View();
+        }
+        
+        public ActionResult RegisterEvent()
+        {
+
+            ViewBag.user = "entity";
+            ViewBag.register_event = "active";
+            
+            return View();
+        }
+    }
+}

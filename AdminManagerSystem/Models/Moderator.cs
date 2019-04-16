c948b49a35585b9ca02af9ff7c8ca189690000c9
kaspary <joaopedrokaspary@hotmail.com>
2019-04-16 10:33:46 -0300

    commit

--- AdminManagerSystem/Models/Moderator.cs
+++ AdminManagerSystem/Models/Moderator.cs
@@ -18,7 +18,7 @@ namespace AdminManagerSystem.Models
         [Display(Name = "Username")]
         [Required(ErrorMessage = "{0} deve ser informado")]
         [StringLength(20, MinimumLength = 4, ErrorMessage = "{0} deve ter entre {1} e {2} caractéres")]
-        public string Usename { get; set; }
+        public string Username { get; set; }
         
         
         [Display(Name = "Nome")]
@@ -35,15 +35,15 @@ namespace AdminManagerSystem.Models
         
         [Required]
         [Display(Name = "Senha")]
-        [DataType(DataType.Password)]
         [StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} caracteres.", MinimumLength = 8)]
         [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "As senhas devem ter pelo menos oito caracteres e conter pelo menos três das seguintes opções: maiúscula (A a Z), minúscula (a-z), número (0 a 9) e caractere especial (por exemplo, @ # $% ^ & *)")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         
-        [DataType(DataType.Password)]
         [Display(Name = "Confirme a Senha")]
         [Compare("Password", ErrorMessage = "As senhas não coincidem.")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
         
         
